56f30e141fb44140a7a4328c271fc94424acbd3f
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2023-06-16 21:33:20 +0000

    add

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let year = prompt('In which year was ECMAScript-2015 specification published?', '');
 
+if (year == 2015) alert( 'You are right!' );
+if (year == 2015) {
+    alert( "That's correct!" );
+    alert( "You're so smart!" );
+  }
+  
