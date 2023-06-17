58a1a78ce23c624bbc81483125d4d185a87ba152
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2023-06-17 07:11:39 +0000

    add

--- index.js
+++ index.js
@@ -68,3 +68,19 @@ if (login == 'Employee') {
 } else {
   message = '';
 }
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
