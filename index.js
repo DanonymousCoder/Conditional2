362e8383a83bd4cc23bcf1a99fe4c7280f29f3dc
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2023-06-17 07:18:34 +0000

    JS

--- index.js
+++ index.js
@@ -84,3 +84,15 @@ switch (a) {
   default:
     alert( "I don't know such values" );
 }
+
+let a = "1";
+let b = 0;
+
+switch (+a) {
+  case b + 1:
+    alert("this runs, because +a is 1, exactly equals b+1");
+    break;
+
+  default:
+    alert("this doesn't run");
+}
