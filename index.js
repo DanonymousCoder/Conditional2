5d19dc96169c3e9fa929c17b9d01e90de04b05fb
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2023-06-17 07:21:50 +0000

    add

--- index.js
+++ index.js
@@ -96,3 +96,21 @@ switch (+a) {
   default:
     alert("this doesn't run");
 }
+
+let arg = prompt("Enter a value?");
+switch (arg) {
+  case '0':
+  case '1':
+    alert( 'One or zero' );
+    break;
+
+  case '2':
+    alert( 'Two' );
+    break;
+
+  case 3:
+    alert( 'Never executes!' );
+    break;
+  default:
+    alert( 'An unknown value' );
+}
