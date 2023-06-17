124e93008891695983f3030b6eb95a49a0054ca9
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2023-06-17 07:37:02 +0000

    JS

--- index.js
+++ index.js
@@ -125,3 +125,16 @@ else if (browser == 'Chrome', 'Safari', 'Opera', 'Firefox') {
 else {
   alert("We hope this page looks ok");
 }
+
+let a = +prompt('a?', '');
+switch (a) {
+  case 'a == 0':
+    alert('0');
+    break;
+  case 'a == 1':
+    alert('1');
+    break;
+  case 'a == 2 || a == 3':
+    alert('2,3');
+    break;
+}
