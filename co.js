c74036e019778fc38436564ab759a5a9af0c285a
Ridwan <aladebaruwa@gmail.com>
2023-08-20 15:55:37 +0100

    add

--- co.js
+++ co.js
@@ -9,5 +9,7 @@ function setWeather() {
   if (choice === "sunny") {
     para.textContent = "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
   }
-  
+  if (choice === "rainy") {
+    para.textContent = 
+  }
 }
