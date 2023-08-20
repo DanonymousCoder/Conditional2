1bd4685d6ec461d09bb3e2996e3e312758ba4bfd
Ridwan <aladebaruwa@gmail.com>
2023-08-20 15:57:03 +0100

    add

--- co.js
+++ co.js
@@ -12,5 +12,8 @@ function setWeather() {
   else if (choice === "rainy") {
     para.textContent =  "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
   }
+  else if (choice === "snowing") {
+    para.textContent =  "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+  }
   
 }
