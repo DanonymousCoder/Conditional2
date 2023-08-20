b289c557bd34906028c9d02f25a1e6c1b632a688
Ridwan <aladebaruwa@gmail.com>
2023-08-20 15:55:08 +0100

    add

--- co.js
+++ co.js
@@ -6,5 +6,8 @@ select.addEventListener("change", setWeather);
 function setWeather() {
   const option = select.value;
   
-  if (choice === "")
+  if (choice === "sunny") {
+    para.textContent = "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+  }
+  
 }
